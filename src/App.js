3b9430fc7907c3dfeb78c8e1e9a5f0bfdea094a8
Muritala Ahmed <muritalaahmed405@gmail.com>
2024-10-04 11:09:31 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
 function App() {
+const [advice, setAdvice]= useState()
+
+  const fetchAdvice = async () => {
+   try{
+    const response = await fetch('https://api.adviceslip.com/advice');
+    const data = await response.json();
+    setAdvice(data.slip.advice);
+   }catch(err){
+    console.error(err)
+   }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app ">
+    <div className="card">
+        <p className="heading">{advice || "Click the button to generate random quotes"}</p>
+        <button className="button" onClick={fetchAdvice}>
+            <span>Generate quote</span>
+        </button>
     </div>
+</div>
   );
 }
 
